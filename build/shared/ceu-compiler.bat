7b74c7c632b47708ac09e65a9b2ecb14560e504f
maged <magedrifaat@gmail.com>
2020-03-20 22:57:29 +0200

    Improved batch file syntax

--- build/shared/ceu-compiler.bat
+++ build/shared/ceu-compiler.bat
@@ -6,17 +6,12 @@ set ARD_BOARD=%3
 set ARD_ARCH=%4
 set ARD_PORT=%5
 
+cd /d %~dp0/../repos/ceu-arduino
 
-cd /d D:\Programs\ceu-maker-windows\repos\ceu-arduino
-
-if "%UPLOAD%" NEQ "true" goto :2
-..\..\mingw\bin\make c CEU_SRC=%CEU_SRC% ARD_BOARD=%ARD_BOARD% ARD_PORT=%ARD_PORT%
-goto :3
-
-:2
-..\..\mingw\bin\make ceu CEU_SRC=%CEU_SRC% ARD_BOARD=%ARD_BOARD% ARD_PORT=%ARD_PORT%
-
-:3
-@echo off
+IF "%UPLOAD%"=="true" (
+    ..\..\mingw\bin\make c CEU_SRC=%CEU_SRC% ARD_BOARD=%ARD_BOARD% ARD_PORT=%ARD_PORT%
+) ELSE (
+    ..\..\mingw\bin\make ceu CEU_SRC=%CEU_SRC% ARD_BOARD=%ARD_BOARD% ARD_PORT=%ARD_PORT%
+)
 
 exit ERRORLEVEL
