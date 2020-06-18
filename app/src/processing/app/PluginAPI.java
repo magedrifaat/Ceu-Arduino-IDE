9b69cc4da43441d41520bed4679b31a50386081c
maged <magedrifaat@gmail.com>
2020-06-18 22:55:14 +0200

    Implemented API function to open skecthes for examples support

--- app/src/processing/app/PluginAPI.java
+++ app/src/processing/app/PluginAPI.java
@@ -1,6 +1,7 @@
 package processing.app;
 
 import javax.swing.JMenu;
+import java.io.File;
 
 public class PluginAPI {
   private Editor editor;
@@ -11,6 +12,21 @@ public class PluginAPI {
   
   public void addMenu(JMenu newMenu) {
     editor.addMenu(newMenu);
+  }
+  
+  public void openFile(String fileName) {
+    File file = new File(fileName);
+    if (file.exists()) {
+      try {
+        editor.base.handleOpen(file);
+      }
+      catch (Exception ex) {
+        editor.statusError("Error while trying to open the file");
+      }
+    }
+    else {
+      editor.statusError("Couldn't find the file specified");
+    }
   }
 }
   
