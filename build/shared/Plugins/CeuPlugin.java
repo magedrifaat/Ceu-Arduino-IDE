fe49d85e1f40a656925034b9e926650e407c2e66
maged <magedrifaat@gmail.com>
2020-07-25 21:31:02 +0200

    Fixed missing Ceu-Arduino examples

--- build/shared/Plugins/CeuPlugin.java
+++ build/shared/Plugins/CeuPlugin.java
@@ -33,31 +33,12 @@ public class CeuPlugin implements Plugin {
 
   JMenu createSubmenu(String menuName, String folderName, String pluginPath, PluginAPI pluginAPI) {
     JMenu subMenu = new JMenu(menuName);
-
-    JMenu examples = new JMenu("Examples");
     
-    // TODO: implement support for recursive search for ceu-arduino examples
     String examplesPath = pluginPath + "/CeuPluginData/" + folderName + "/Examples/";
     File examplesDir = new File(examplesPath);
-    for (File file : examplesDir.listFiles()) {
-      if (file.isDirectory()) {
-        File exampleFile = new File(file, file.getName() + ".ceu");
-        if (exampleFile.exists()) {
-          JMenuItem example = new JMenuItem(file.getName());
-          example.addActionListener((e) -> pluginAPI.openFile(exampleFile.getAbsolutePath()));
-          examples.add(example);
-        }
-      }
-      else if (file.getName().endsWith(".ceu")) {
-        JMenuItem example = new JMenuItem(file.getName().substring(0, file.getName().length() - 4));
-        example.addActionListener((e) -> pluginAPI.openFile(file.getAbsolutePath()));
-        examples.add(example);
-      }
-    }
-    
+    JMenu examples = recursiveExamplesMenu(examplesDir, examplesDir.getName(), pluginAPI);
     
     JMenu libraries = new JMenu("Include Library");
-    
     String libsPath = pluginPath + "/CeuPluginData/" + folderName + "/Include/";
     File libsDir = new File(libsPath);
     for (File file : libsDir.listFiles()) {
@@ -72,5 +53,38 @@ public class CeuPlugin implements Plugin {
     subMenu.add(libraries);
     
     return subMenu;
+  }
+  
+  /**
+   * Recursively follow sub-folders to find all examples and load them as a menu tree.
+   */
+  JMenu recursiveExamplesMenu(File exampleDir, String parentName, PluginAPI pluginAPI) {
+    JMenu rootMenu = new JMenu(parentName);
+    for (File file : exampleDir.listFiles()) {
+      if (file.isDirectory()) {
+        File subFile = new File(file, file.getName() + ".ceu");
+        if (subFile.exists()) {
+          JMenuItem example = new JMenuItem(file.getName());
+          example.addActionListener((e) -> pluginAPI.openFile(subFile.getAbsolutePath()));
+          rootMenu.add(example);
+        }
+        else {
+          // Skip over an intermediate "examples" folder
+          if (file.getName().equals("examples")) {
+            rootMenu = recursiveExamplesMenu(file, parentName, pluginAPI);
+          }
+          else {
+            rootMenu.add(recursiveExamplesMenu(file, file.getName(), pluginAPI));
+          }
+        }
+      }
+      else if(file.getName().endsWith(".ceu")) {
+        JMenuItem example = new JMenuItem(file.getName().substring(0, file.getName().length() - 4));
+        example.addActionListener((e) -> pluginAPI.openFile(file.getAbsolutePath()));
+        rootMenu.add(example);
+      }
+    }
+
+    return rootMenu;
   }
 }
